c92b70c23f5bb5eb872c3caf1fe53f3e2875be10
Akasus <akasus.nevelas@gmail.com>
2023-02-24 20:38:52 +0100

    initial commit

---
+++ MODNAME/SRC/CLIENT/MainClient.cs
@@ -0,0 +1,15 @@
+using System;
+using LogicAPI.Client;
+using UnityEngine;
+using System.Linq;
+
+
+namespace $safeprojectname$.Client
+{
+    public class ClientMain : ClientMod
+    {
+        protected override void Initialize()
+        {
+        }
+    }
+}
---
+++ MODNAME/SRC/SERVER/MainServer.cs
@@ -0,0 +1,13 @@
+using System;
+using LogicAPI.Server;
+
+
+namespace $safeprojectname$.Server
+{
+    public class ServerMain : ServerMod
+    {
+        protected override void Initialize()
+        {
+        }
+    }
+}
